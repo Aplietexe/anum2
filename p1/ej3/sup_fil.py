faae1b06485db261b18ed7b24303313abc8574d3
Pietro Palombini <pietropalo@icloud.com>
2024-08-28 21:12:43 -0300

    Add back row substitution

---
+++ p1/ej3/sup_fil.py
@@ -0,0 +1,22 @@
+import numpy as np
+from numpy.typing import NDArray
+from tests import test_sup_solve
+
+Arr = NDArray[np.float64]
+
+
+def sol_trsupfil(A: Arr, b: Arr) -> Arr | None:
+    n = A.shape[0]
+    if A.shape != (n, n) or b.shape != (n,):
+        raise ValueError("Invalid shapes")
+    if np.any(A.diagonal() == 0):
+        return None
+    x = np.zeros(n)
+    for i in range(n - 1, -1, -1):
+        x[i] = (b[i] - x[i + 1 :] @ A[i, i + 1 :]) / A[i, i]
+    return x
+
+
+if __name__ == "__main__":
+    t = test_sup_solve(sol_trsupfil)
+    print(t)
