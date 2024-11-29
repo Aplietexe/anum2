3a210e24de91920ff699c058a89908553dde4be8
Pietro Palombini <pietropalo@icloud.com>
2024-11-29 19:19:30 -0300

    Reorganize matrix inverse code

--- code/inv.py
+++ code/inv.py
@@ -1,33 +1,33 @@
 import numpy as np
 from numpy.typing import NDArray
 
-from p1.ej3.inf_fil import sol_trinffil
-from p1.ej3.sup_fil import sol_trsupfil
-
-from ..ej9.lu_p import dlup
-from .tests import test_inv
+from .factorizations.lu_pivoting import lu
+from .square.triangular.lower_row import solve_lower_triangular
+from .square.triangular.upper_row import solve_upper_triangular
 
 Arr = NDArray[np.float64]
 
 
-def inv_lu(A: Arr) -> Arr | None:
+def inv(A: Arr) -> Arr | None:
     """
     Given a square matrix A, computes its inverse if it exists.
     Overwrites A with the inverse and returns it.
     Returns None if A is not invertible.
     """
     n = A.shape[0]
-    L, U, P = dlup(A)
+    L, U, P = lu(A)
     if np.isclose(np.prod(np.diag(U)), 0):
         return None
 
     for i in range(n):
-        y = sol_trinffil(L, P[:, i])
-        A[:, i] = sol_trsupfil(U, y)
+        y = solve_lower_triangular(L, P[:, i])
+        A[:, i] = solve_upper_triangular(U, y)
 
     return A
 
 
 if __name__ == "__main__":
-    t = test_inv(inv_lu)
+    from tests.inv import test_inv
+
+    t = test_inv(inv)
     print(t)
